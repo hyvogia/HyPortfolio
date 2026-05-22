492532bff8f41cac906c05aebd090630929cadc3
Hy Vo <vogiahy@gmail.com>
2026-05-21 23:39:45 -0400

    Modify services into experiences

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -12,24 +12,26 @@ function Features() {
                     <div className="row justify-content-center">
                         <div className="col-lg-8 text-center">
                             <div className="main_title">
-                                <h2>current concentrations</h2>
+                                <h2>experiences</h2>
                             </div>
                         </div>
                     </div>
                     <div className="row feature_inner">
                         <div className="col-lg-3 col-md-6">
                             <div className="feature_item">
-                                <img src={s1} alt=""></img>
-                                <h4>Software App</h4>
+                                <img src="/src/img/brands/niagaracollege.svg" className="feature-logo" alt="" />
+                                <h4>Peer Tutor</h4>
+                                <p>Part-time @ Niagara College Canada</p>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
                             <div className="feature_item">
-                                <img src={s2} alt=""></img>
-                                <h4>Data Analysis</h4>
+                                <img src="/src/img/brands/ridleycollege.png" className="feature-logo" alt="" />
+                                <h4>Technical Data Analyst</h4>
+                                <p>Full-time & Ridley College Canada</p>
                             </div>
                         </div>
-                        <div className="col-lg-3 col-md-6">
+                        {/* <div className="col-lg-3 col-md-6">
                             <div className="feature_item">
                                 <img src={s3} alt=""></img>
                                 <h4>Web Development</h4>
@@ -40,7 +42,7 @@ function Features() {
                                 <img src={s4} alt=""></img>
                                 <h4>Machine Learning</h4>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
